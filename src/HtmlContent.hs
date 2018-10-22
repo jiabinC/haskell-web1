f90c0b87a3ec26629d435269d29d602e0e57edd5
jiabinC <1402021538@qq.com>
2018-10-22 12:50:18 +0800

    add HtmlContent

---
+++ src/HtmlContent.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
+import Servant ( QueryParam
+               , PlainText
+               , Get
+               , Proxy(..)
+               , type (:>)      -- Syntax for importing type operator
+               , type (:<|>)
+               , (:<|>)(..)
+               , Accept(..)
+               , MimeRender(..)
+               )
+import Servant.Server (Handler, Server, Application, serve)
+import Network.Wai.Handler.Warp (run)
+import Control.Monad.IO.Class (liftIO)
+import Data.ByteString.Lazy.Char8 as C
+
+
+data HTML
+
+instance Accept HTML where 
+    contentType _ = "text/html"
+instance MimeRender HTML String where
+    mimeRender _ val = C.pack val
+instance MimeRender HTML Int where
+    mimeRender _ val = C.pack $ show val
+
+type HTMLApi = "name" :> Get '[HTML] string
+                :<|> "age" :> Get '[HTML] Int
+
+htmlApi :: Proxy HTMLApi
+htmlApi = Proxy
+
+handlerName :: Handler String
+handlerName = return "abin"
+
+handlerAge :: Handler Int
+handlerAge = return 20
+
+server :: Server HTMLApi
+server = handlerName :<|> handlerAge
+
+app :: Application
+app = serve htmlApi server
+
+mainFn :: IO ()
+mainFn = run 4002 app
